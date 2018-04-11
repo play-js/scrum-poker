e96345208a1fb524ecc4db13500281a38d2881d2
qvil <tshyeon@seowonintech.co.kr>
2018-04-11 15:58:40 +0900

    Add Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 // custom
 import "styles/index.css";
 import App from "containers/App";
 import reducer from "reducers";
 
-const store = createStore(reducer);
+const composeEnhancers =
+  process.env.NODE_ENV !== "production"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+const store = createStore(reducer, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
